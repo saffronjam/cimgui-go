e62c0edeabea4479ccc45e98d19101bb8771f689
saffronjam <own.emka@gmail.com>
2025-05-29 22:21:13 +0200

    Remove unused SFML includes from sfml_backend.h

--- backend/sfml_backend/sfml_backend.h
+++ backend/sfml_backend/sfml_backend.h
@@ -1,14 +1,6 @@
 #ifndef IMGUI_SFML_H
 #define IMGUI_SFML_H
 
-#include "../../thirdparty/SFML/include/SFML/Graphics/Color.hpp"
-#include "../../thirdparty/SFML/include/SFML/Graphics/Rect.hpp"
-#include "../../thirdparty/SFML/include/SFML/System/Time.hpp"
-#include "../../thirdparty/SFML/include/SFML/System/Vector2.hpp"
-#include "../../thirdparty/SFML/include/SFML/Window/Joystick.hpp"
-
-#include <optional>
-
 namespace sf
 {
     class Event;
