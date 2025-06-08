8d42ee806437d480bd823c995ce274b82247902d
saffronjam <own.emka@gmail.com>
2025-06-08 16:02:15 +0200

    Fix lib imports

--- lib.go
+++ lib.go
@@ -5,7 +5,4 @@ package imgui
 
 import (
 	_ "github.com/saffronjam/cimgui-go/lib/linux/x64"
-	_ "github.com/saffronjam/cimgui-go/lib/macos/arm64"
-	_ "github.com/saffronjam/cimgui-go/lib/macos/x64"
-	_ "github.com/saffronjam/cimgui-go/lib/windows/x64"
 )
